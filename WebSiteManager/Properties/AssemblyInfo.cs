2024d0b27238b4de45e324d5f32b08d40e6f2738
rocky <rocklau83@gmail.com>
2014-02-14 12:53:14 +0800

    sample code

---
+++ WebSiteManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebSiteManager")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebSiteManager")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("04504117-6f58-4ce6-aabd-8b00a362acfd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebSiteManager/SiteAdmin.aspx.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Web.Administration;
+using Microsoft.Web.Management;
+
+namespace WebSiteManager
+{
+    public partial class SiteAdmin : System.Web.UI.Page
+    {
+        ServerManager IISManager = new ServerManager();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        protected void AlterWebConfig()
+        {
+            Configuration appHost = IISManager.GetApplicationHostConfiguration();
+            ConfigurationSection configPaths = appHost.GetSection("connectionstring");
+            using (ServerManager serverManager = new ServerManager())
+            {
+                Configuration config = serverManager.GetWebConfiguration("TestSite");
+
+                ConfigurationSection directoryBrowseSection =
+                    config.GetSection("system.webServer/directoryBrowse");
+
+
+                if ((bool)directoryBrowseSection["enabled"] != true)
+                {
+                    directoryBrowseSection["enabled"] = true;
+                }
+
+                serverManager.CommitChanges();
+            }
+          
+        }
+        protected void CreateSite(string sitename,string domain,string path)
+        { 
+
+            IISManager.Sites.Add(sitename, "http", domain, path);
+            IISManager.CommitChanges();
+        }
+        protected void RemoveSite(string sitename, string appoolname)
+        { 
+        
+         IISManager.Sites[sitename].Applications.Remove(this.IISManager.Sites[sitename].Applications[0]);
+         IISManager.Sites.Remove(this.IISManager.Sites[sitename]);
+         IISManager.ApplicationPools.Remove(this.IISManager.ApplicationPools[appoolname]);
+         IISManager.CommitChanges();
+        }
+        protected void RoleControl()
+        {
+            Configuration config = IISManager.GetApplicationHostConfiguration();
+            ConfigurationSection anonymousAuthenticationSection = config.GetSection("system.webServer/security/authentication/anonymousAuthentication", "SiteName");
+            anonymousAuthenticationSection["enabled"] = true;
+            anonymousAuthenticationSection["userName"] = @"IUSR_" + "username";
+            anonymousAuthenticationSection["password"] = @"" + "password";
+            IISManager.CommitChanges();
+        }
+        protected void CreatPool(string appoolname)
+        {
+            ApplicationPool newPool = IISManager.ApplicationPools[appoolname];
+            if (newPool == null)
+            {
+                IISManager.ApplicationPools.Add(appoolname);
+                newPool = IISManager.ApplicationPools[appoolname];
+                newPool.Name = appoolname;
+                newPool.ManagedPipelineMode = ManagedPipelineMode.Integrated;
+                newPool.ManagedRuntimeVersion = "v4.0";
+            }
+        }
+
+        protected void AlterPool(string sitename,string appoolname)
+        {
+            IISManager.Sites[sitename].Applications["/"].ApplicationPoolName = appoolname;
+        }
+        private void BindURL(string sitename, string domain)
+        {
+            string str =  IISManager.Sites[sitename].Bindings[0].Host.Split(new char[] { '.' })[0];
+            string bindingInformation = "*:80:" + str + "." + domain;
+            IISManager.Sites[sitename].Bindings.Add(bindingInformation, "http");
+             IISManager.CommitChanges();
+        }
+        void getListOfIIS()
+        {
+           // this.listBox1.Items.Clear();
+            string StateStr = "";
+            for (int i = 0; i < IISManager.Sites.Count; i++)
+            {
+
+                switch (IISManager.Sites[i].State)
+                {
+                    case ObjectState.Started:
+                        {
+                            StateStr = "正常"; break;
+                        }
+                    case ObjectState.Starting:
+                        {
+                            StateStr = "正在启动"; break;
+                        }
+                    case ObjectState.Stopping:
+                        {
+                            StateStr = "正在关闭"; break;
+                        }
+                    case ObjectState.Stopped:
+                        {
+                            StateStr = "关闭"; break;
+                        }
+                }
+              //  this.listBox1.Items.Add(IISManager.Sites[i].Name + "【" + StateStr + "】");
+            }
+        }
+    
+    }
+}
